e0b27cb51e20f0291d6c46e0bfbb251738e2547e
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2020-02-09 18:52:33 -0800

    Update model to add projects, resources, and tasks tables. Need to add 1:1 table

--- data/migrations/20200209181250_create-tables.js
+++ data/migrations/20200209181250_create-tables.js
@@ -1,8 +1,44 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+    // Table of projects
+    await knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments();
+            tbl.string('project_name', 128)
+                .notNullable();
+            tbl.string('project_description', 255);
+            tbl.boolean('completed?')
+                .notNullable()
+                .defaultTo(false);
+        });
+
+    // Table of resources
+    await knex.schema
+        .createTable('resources', tbl => {
+            tbl.increments();
+            tbl.string('resource_name', 128)
+                .notNullable();
+            tbl.string('description', 255);
+        });
+
+    // Table of tasks
+    await knex.schema
+        .createTable('tasks', tbl => {
+            tbl.increments();
+            tbl.string('description', 255)
+                .notNullable();
+            tbl.string('notes')
+            tbl.boolean('task_completed?')
+                .notNullable()
+                .defaultTo('false');
+        });
 };
 
-exports.down = function(knex) {
-  
+
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
 };
